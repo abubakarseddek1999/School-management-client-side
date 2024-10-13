4b4d61f00b46f0de537f6008443aae4c9afcd5f4
abubakarseddek1999 <abubakarseddek1999@gmail.com>
2024-10-13 17:16:26 +0600

    change subscibe button

--- src/Pages/Home/Subscribe/Subscribe.jsx
+++ src/Pages/Home/Subscribe/Subscribe.jsx
@@ -1,4 +1,5 @@
 import backgroundImage from '../../../assets/image/school.jpg';
+import SubscribeBtn from '../../../Components/Subscibebtn/SubscribeBtn';
 
 const Subscribe = () => {
     return (
@@ -12,10 +13,9 @@ const Subscribe = () => {
             className='w-full h-[500px] flex justify-center items-center'>
             <div>
 
-                <p className='text-3xl md:text-5xl lg:text-6xl font-bold text-white text-center p-5'>Join With Us</p>
-                <div className='flex justify-center'>
-                    <input className='p-2' type="text" placeholder='Enter Your email' name="" id="" />
-                    <button className='bg-[#FC724D] hover:bg-[#ff714d] text-white p-2'> Subscribe</button>
+                <p className='text-3xl md:text-5xl lg:text-6xl font-bold text-white text-center p-5 '>Join With Us</p>
+                <div className='flex justify-center mb-5'>
+                     <SubscribeBtn></SubscribeBtn>
                 </div>
             </div>
 
