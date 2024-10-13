488aaf8d9b15e8e60445a4a20772a6fedb4d22f7
abubakarseddek1999 <abubakarseddek1999@gmail.com>
2024-10-13 17:31:10 +0600

    add some link on footer

--- src/Share/Footer/Footer.jsx
+++ src/Share/Footer/Footer.jsx
@@ -2,32 +2,44 @@
 
 const Footer = () => {
     return (
-        <div className="bg-[#2A7CC7] ">
+        <div className="bg-[#2A7CC7] md:h-[400px] p-5">
             <footer className=" flex flex-col md:flex-row justify-between  max-w-[1400px] mx-auto p-5 text-base-content ">
 
                 <div className="text-white flex flex-col mb-5">
-                    <header className="font-bold">Services</header>
+                    <header className="text-xl font-bold">Services</header><a className="cursor-pointer">Admissions</a>
+                    <a className="cursor-pointer">Academics</a>
                     <a className="cursor-pointer">Branding</a>
                     <a className="cursor-pointer">Design</a>
                     <a className="cursor-pointer">Marketing</a>
                     <a className="cursor-pointer">Advertisement</a>
+                    <a className="cursor-pointer">Student Counseling</a>
+                    <a className="cursor-pointer">E-learning Platform</a>
+                    <a className="cursor-pointer">Online Fee Payment</a>
+                    <a className="cursor-pointer">Transportation</a>
                 </div>
                 <div className="text-white flex flex-col mb-5">
-                    <header className="font-bold">Company</header>
+                    <header className="font-bold text-xl">Company</header>
                     <a className="cursor-pointer">About us</a>
                     <a className="cursor-pointer">Contact</a>
                     <a className="cursor-pointer">Jobs</a>
                     <a className="cursor-pointer">Press kit</a>
+                    <a className="cursor-pointer">Student Portal</a>
+                    <a className="cursor-pointer">Faculty</a>
+                    <a className="cursor-pointer">Library</a>
                 </div>
                 <div className="text-white flex flex-col mb-5">
-                    <header className="font-bold">Legal</header>
+                    <header className="font-bold text-xl">Legal</header>
                     <a className="cursor-pointer">Terms of use</a>
                     <a className="cursor-pointer">Privacy policy</a>
                     <a className="cursor-pointer">Cookie policy</a>
+                    <a className="cursor-pointer">Disclaimer</a>
+                    <a className="cursor-pointer">Code of Conduct</a>
+                    <a className="cursor-pointer">Non-discrimination Policy</a>
+                    <a className="cursor-pointer">Data Protection</a>
                 </div>
 
                 <nav className="text-white ">
-                    <header className="font-bold">Social</header>
+                    <header className="font-bold text-xl">Social</header>
                     <div className="flex gap-4">
                         <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current cursor-pointer"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path></svg></a>
                         <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current cursor-pointer"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path></svg></a>
