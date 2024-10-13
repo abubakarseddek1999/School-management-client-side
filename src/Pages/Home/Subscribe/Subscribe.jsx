55a40905e27c3fed98549a78c63ede24fc9d7f0b
abubakarseddek1999 <abubakarseddek1999@gmail.com>
2024-10-13 23:39:59 +0600

    set opacity on subscribe section

--- src/Pages/Home/Subscribe/Subscribe.jsx
+++ src/Pages/Home/Subscribe/Subscribe.jsx
@@ -3,22 +3,26 @@ import SubscribeBtn from '../../../Components/Subscibebtn/SubscribeBtn';
 
 const Subscribe = () => {
     return (
-        <div style={{
-            backgroundImage: `url(${backgroundImage})`,
-            backgroundRepeat: "no-repeat",
-            // backgroundSize: 'cover',
-            backgroundPosition: 'center',
-            backgroundSize: '100% 500px',
-        }}
-            className='w-full h-[500px] flex justify-center items-center'>
-            <div>
+        <div className='relative w-full h-[500px]'>
+            <div style={{
+                backgroundImage: `url(${backgroundImage})`,
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: 'center',
+                backgroundSize: '100% 500px',
+                // filter: 'brightness(0.5)',
+            }}
+            className='absolute inset-0 z-0'>
+            </div>
+            <div className='absolute inset-0 bg-black opacity-50 z-10'></div> 
 
-                <p className='text-3xl md:text-5xl lg:text-6xl font-bold text-white text-center p-5 '>Join With Us</p>
-                <div className='flex justify-center mb-5'>
-                     <SubscribeBtn></SubscribeBtn>
+            <div className='relative z-20 flex justify-center items-center h-full'>
+                <div>
+                    <p className='text-3xl md:text-5xl lg:text-6xl font-bold text-white text-center p-5'>Join With Us</p>
+                    <div className='flex justify-center mb-5'>
+                        <SubscribeBtn />
+                    </div>
                 </div>
             </div>
-
         </div>
     );
 };
