5327e86aa0c4b1657e5713637f5a9a7e7ff1db59
abubakarseddek1999 <abubakarseddek1999@gmail.com>
2024-12-24 11:06:13 +0600

    deshboard menu icon size increese

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -3,72 +3,75 @@ import logo from "../assets/image/logo.png"
 import { FaAd, FaCalendar, FaEnvelope, FaHome, FaList, FaUsers, FaUtensils } from "react-icons/fa";
 import { NavLink, Outlet } from "react-router-dom";
 import { RiMenuFold2Fill, RiMenuFold3Fill } from "react-icons/ri";
+import { PiStudentFill } from "react-icons/pi";
+import { MdBugReport } from "react-icons/md";
 
 const Dashboard = () => {
     const [isExpanded, setIsExpanded] = useState(false);
-
     const toggleSidebar = () => setIsExpanded(!isExpanded);
 
     return (
         <div className="flex">
             {/* dashboard side bar */}
-            <div className={`w-${isExpanded ? '64' : '16'} min-h-screen bg-blue-400 `}>
-                <div className="flex justify-center items-center relative">
-                    
-                    <img className="w-[50px] h-[50px]  mt-1" src={logo} alt="" />
-                    <button onClick={toggleSidebar} className="text-white absolute -right-7">
-                    {isExpanded ?<RiMenuFold3Fill  className="text-3xl text-blue-400 font-bold" />   : <RiMenuFold2Fill  className="text-3xl  text-blue-400 font-bold" /> }
-                    </button>
-                </div>
+            <div className={`w-${isExpanded ? '64' : '17'} min-h-screen bg-blue-400 `}>
+                <NavLink to="/">
+                    <div className="flex justify-center items-center relative cursor-pointer">
+
+                        <img className="w-[50px] h-[50px]  mt-1" src={logo} alt="" />
+                        <button onClick={toggleSidebar} className="text-white absolute -right-7">
+                            {isExpanded ? <RiMenuFold3Fill className="text-3xl text-blue-400 font-bold" /> : <RiMenuFold2Fill className="text-3xl  text-blue-400 font-bold" />}
+                        </button>
+                    </div>
+                </NavLink>
                 <ul className="menu p-2 text-white font-bold text-sm">
                     <li>
                         <NavLink to="/dashboard/adminHome">
-                            <FaHome className="text-2xl" /> {isExpanded && <span>Admin Home</span>}
+                            <FaHome className="text-3xl" /> {isExpanded && <span>Admin Home</span>}
                         </NavLink>
                     </li>
                     <li>
                         <NavLink to="/dashboard/addItems">
-                            <FaUtensils className="text-2xl"/> {isExpanded && <span>Add Student</span>}
+                            <PiStudentFill className="text-3xl" /> {isExpanded && <span>Add Student</span>}
                         </NavLink>
                     </li>
                     <li>
                         <NavLink to="/dashboard/manageItems">
-                            <FaList className="text-2xl" /> {isExpanded && <span>Manage Student</span>}
+                            <FaList className="text-3xl" /> {isExpanded && <span>Manage Student</span>}
                         </NavLink>
                     </li>
                     <li>
                         <NavLink to="/dashboard/review">
-                            <FaAd className="text-2xl"/> {isExpanded && <span>Review</span>}
+                            <FaAd className="text-3xl" /> {isExpanded && <span>Review</span>}
                         </NavLink>
                     </li>
                     <li>
                         <NavLink to="/dashboard/allUsers">
-                            <FaUsers className="text-2xl"/> {isExpanded && <span>All Users</span>}
+                            <FaUsers className="text-3xl" /> {isExpanded && <span>All Users</span>}
                         </NavLink>
                     </li>
                     <li>
                         <NavLink to="/dashboard/teacherMange">
-                            <FaUsers className="text-2xl"/> {isExpanded && <span>Manage Teacher</span>}
+                            <FaUsers className="text-3xl" /> {isExpanded && <span>Manage Teacher</span>}
                         </NavLink>
                     </li>
                     <li>
                         <NavLink to="/dashboard/viewReports">
-                            <FaHome className="text-2xl"/> {isExpanded && <span>View Reports</span>}
+                            <MdBugReport className="text-3xl" /> {isExpanded && <span>View Reports</span>}
                         </NavLink>
                     </li>
                     <li>
                         <NavLink to="/dashboard/history">
-                            <FaCalendar className="text-2xl"/> {isExpanded && <span>Payment History</span>}
+                            <FaCalendar className="text-3xl" /> {isExpanded && <span>Payment History</span>}
                         </NavLink>
                     </li>
                     <li>
                         <NavLink to="/dashboard/paymentHistory">
-                            <FaList className="text-2xl"/> {isExpanded && <span>Payment Real History</span>}
+                            <FaList className="text-3xl" /> {isExpanded && <span>Payment Real History</span>}
                         </NavLink>
                     </li>
                     <li>
-                        <NavLink to="/order/contact">
-                            <FaEnvelope className="text-2xl"/> {isExpanded && <span>Contact</span>}
+                        <NavLink to="/contact">
+                            <FaEnvelope className="text-3xl" /> {isExpanded && <span>Contact</span>}
                         </NavLink>
                     </li>
                 </ul>
